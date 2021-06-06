495a946039b7d5ce5c1e50683a4697a9cfcdddeb
Tuhin Mitra <tuhinmitra190221@gmail.com>
2021-06-06 20:25:21 +0530

    to be used for coursera courses tracking

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import json
-from flask import Flask,render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_migrate import Migrate
 import models
 from models import db
-
 
 app = Flask(__name__)
 app.debug = True
 app.config.from_json('config.json')
 app.secret_key = bytes(app.config['SECRET_KEY'], 'UTF-8')
 db.init_app(app)
+
 migrate = Migrate(app, db)
+
 
 def create_app():
     """Construct the core application."""
@@ -18,27 +19,40 @@ def create_app():
         # Create tables for our models
         db.create_all()
         return app
+
+
 create_app()
+
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
+
 @app.route("/view", methods=['GET'])
 def view_db():
-    data = models.MainDB.query.all()
+    # data = models.MainDB.query.all()
+    data = models.MainDB.query.order_by(models.MainDB.id).all()
     table_name = models.MainDB.__tablename__
     columns = models.MainDB.metadata.tables[table_name].columns.keys()
     return render_template('view.html', table_data=data, columns=columns, table_name=models.MainDB.__tablename__)
+
 
 @app.route("/update", methods=['POST'])
 def update_db():
     data = request.form
     resp_dict = dict(data)  # retrieve data from front-end
+    to_update = resp_dict.copy()
+    del to_update['id']
+    db_update = models.MainDB
+    db.session.query(models.MainDB).filter(models.MainDB.id == int(resp_dict['id'])).update(to_update)
+    db.session.commit()
     print("ROW-DATA:", resp_dict)
-    return json.dumps({'status':'OK'});
 
-@app.route("/add",methods=['GET', 'POST'])
+    return json.dumps({'status': 'OK'})
+
+
+@app.route("/add", methods=['GET', 'POST'])
 def add_data():
     if request.method == "POST":
         form_data = request.form
@@ -51,7 +65,7 @@ def add_data():
             flash("Data Updated successfully!")
             print("redirect url:", request.url)
         return redirect(url_for('add_data'))
-    return render_template('add_data.html', columns = models.MainDB.fields)
+    return render_template('add_data.html', columns=models.MainDB.fields)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -2,37 +2,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class MainDB(db.Model):
     __tablename__ = "view_table_first"
 
     fields = [["id", 'id'],
-            ["Country (or dependency)","Country"],
-            ["Population (2020)","Population"],
-            ["Yearly Change","Yearly_change"],
-            ["Net Change","Net_change"],
-            ["Density (P/Km²)","Density"],
-            ["Land Area (Km²)","Land_area"],
-            ["Migrants (net)","Migrants"],
-            ["Fert. Rate","Fert_rate"],
-            ["Med. Age","Med_age"],
-            ["Urban Pop %","Urban"],
-            ["World Share","world_share"]]
+              ["Course Name", 'course_name'],
+              ["Registered Acc. (email)", 'registered_eml'],
+              ["Registered on", "registered_date"],
+              ["Due date", 'due_date']]
     id = db.Column(db.Integer(), primary_key=True)
-    Country=db.Column(db.String())
-    Population=db.Column(db.String())
-    Yearly_change=db.Column(db.String())
-    Net_change=db.Column(db.String())
-    Density=db.Column(db.String())
-    Land_area=db.Column(db.String())
-    Migrants=db.Column(db.String())
-    Fert_rate=db.Column(db.String())
-    Med_age=db.Column(db.String())
-    Urban=db.Column(db.String())
-    world_share=db.Column(db.String())
-    
+    course_name = db.Column(db.String(), unique=True)
+    registered_date = db.Column(db.String())
+    registered_eml = db.Column(db.String())
+    due_date = db.Column(db.String())
+
     def __init__(self, **kwargs):
         for field_name in self.fields[1:]:
             setattr(self, field_name[1], kwargs.get(field_name[1]))
-    
+
     def __repr__(self):
-        return f"{self.Country} - {self.Population}"
+        return f"{self.course_name} - {self.registered_eml}"
