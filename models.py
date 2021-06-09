2f3ed13fcdba041e0ff1ea84ebc1b81dcdf54084
Tuhin Mitra <tuhinmitra190221@gmail.com>
2021-06-09 11:12:30 +0530

    new column added

--- models.py
+++ models.py
@@ -4,18 +4,23 @@ db = SQLAlchemy()
 
 
 class MainDB(db.Model):
+    """
+    custom model for the database to create and display.
+    """
     __tablename__ = "view_table_first"
 
     fields = [["id", 'id'],
               ["Course Name", 'course_name'],
               ["Registered Acc. (email)", 'registered_eml'],
               ["Registered on", "registered_date"],
-              ["Due date", 'due_date']]
+              ["Due date", 'due_date'],
+              ["Complete Status", "complete_status"]]
     id = db.Column(db.Integer(), primary_key=True)
     course_name = db.Column(db.String(), unique=True)
     registered_date = db.Column(db.String())
     registered_eml = db.Column(db.String())
     due_date = db.Column(db.String())
+    complete_status = db.Column(db.String())
 
     def __init__(self, **kwargs):
         for field_name in self.fields[1:]:
---
+++ start_flask.sh
@@ -0,0 +1,4 @@
+export FLASK_APP=app.py
+export FLASK_ENV=development
+export FLASK_DEBUG=1
+flask run
